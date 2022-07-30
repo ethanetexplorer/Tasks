ea05b915628bbb59e90aaca354eb250ff70006ba
Ethan Lim <ethanolicacid@outlook.com>
2022-07-30 09:47:38 +0800

    Initial Commit

---
+++ Tasks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tasks
+//
+//  Created by Ethan Lim on 30/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Ethan Lim on 30/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
