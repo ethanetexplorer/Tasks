43de6aaa99c605f75f285ba3f67881103a0f94fe
Ethan Lim <ethanolicacid@outlook.com>
2022-07-30 09:58:35 +0800

    Gibberish and repeated items in sections

--- Tasks/ContentView.swift
+++ Tasks/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List{
+            Section("Section A: gibberish"){
+                Text("aerbug")
+                    .padding()
+                Text("asdf")
+                    .padding()
+            }
+            Section("Section B: repeated items"){
+                ForEach(0..<5){ item in
+                    Text("Repeated items")
+                        .padding()
+            }
+            }
+        }
     }
 }
 
